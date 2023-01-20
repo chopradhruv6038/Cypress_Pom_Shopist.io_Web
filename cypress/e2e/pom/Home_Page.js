1e61fe472a41b1cf607369bf2b61e39af2627d01
chopradhruv6038 <chopradhruv6038@gmail.com>
2023-01-20 19:20:14 +0400

    First Commit-Added vldtns for profile/edit profile

---
+++ cypress/e2e/pom/Home_Page.js
@@ -0,0 +1,21 @@
+export class HomePage{
+
+ myProfileLink = '.profile > .menu-item-large-container > .menu-item-large';
+
+
+
+
+
+
+
+clickMyProfileLink(){
+
+cy.get(this.myProfileLink).click();
+
+
+}
+
+
+
+
+}
