8ea0959d231b522afc8ac0b4716c7426e38e30dc
chopradhruv6038 <chopradhruv6038@gmail.com>
2023-01-23 18:53:31 +0400

    assertn Sold out items alrt bx txt and cnt Btn

---
+++ cypress/e2e/Tests/SoldOutItemsValidationTests.cy.js
@@ -0,0 +1,42 @@
+import { BasePage } from "../pom/Base_Page";
+import { LightingPage } from "../pom/Lighting_Page";
+import { ChairsPage } from "../pom/Chairs_Page";
+
+
+const basepage = new BasePage();
+const lightingpage = new LightingPage();
+const chairspage = new ChairsPage();
+
+describe('This suite will test the alert box and text of sold out items on clicking', function(){
+
+beforeEach(()=> {
+
+basepage.loadUrl('/');
+
+basepage.setViewPortTo1280X720();
+
+})
+
+
+it('This suite will test the alert box and its Text on clicking a sold out item, it will then click on continue btn, will validate that user is back on product page and will again click on a product which is sold out and assert the sold out text',()=> {
+
+chairspage.clickChairsLink();
+
+chairspage.ClickUpholsteredWhiteChairSoldOut();
+
+chairspage.AssertSoldOutAlertBoxText();
+
+chairspage.clickContinueBtnOnSoldOutAlert();
+
+chairspage.assertUserOnChairProductsPage(); // assering user is back on chair products page.
+
+chairspage.clickUpholsteredBlackBarStoolSoldOut();
+
+chairspage.AssertSoldOutAlertBoxText();
+
+})
+
+
+
+
+})
--- cypress/e2e/pom/Chairs_Page.js
+++ cypress/e2e/pom/Chairs_Page.js
@@ -1,3 +1,4 @@
+
 export class ChairsPage{
 
 chairsLink = '.chairs > .menu-item-large-container > .menu-item-large';
@@ -63,6 +64,17 @@ expectedUpholsteredBlackBarStoolStock = 'Sold out';
 expectedUpholsteredBlackBarStoolDescription = 'Upholstered Black Barstool';
 expectedUpholsteredBlackBarStoolPrice = '$210.00';
 expectedUpholsteredBlackBarStoolIMGSrc = '_nuxt/img/9.9733e92.jpg';
+
+soldOutItemAlertBox = '.modal-sold-out--is-open > .modal-sold-out-content';
+
+expectedSoldOutItemAlertBoxText01 = 'Oops! This item is sold out.'
+expectedSoldOutItemAlertBoxText02 = 'It can'
+expectedSoldOutItemAlertBoxText03 = 't be added to your cart.'
+expectedSoldOutItemAlertBoxText04 = 'We'
+expectedSoldOutItemAlertBoxText05 = 'll let you know when this item is back in stock.'
+expectedSoldOutItemAlertBoxText06 = 'Continue shopping'
+
+ContinueBTnOnSoldOutAlert = '.modal-sold-out--is-open > .modal-sold-out-content > .modal-button';
 
 
 clickChairsLink(){
@@ -140,6 +152,43 @@ assertDescPriceStockIMGUpholsteredBlackBarStool(){
 cy.get(this.upholsteredBlackBarStool).should('have.text', ' ' + this.expectedUpholsteredBlackBarStoolStock + ' ' + this.expectedUpholsteredBlackBarStoolDescription + ' ' 
 + this.expectedUpholsteredBlackBarStoolPrice)
 .find('img').should('have.attr', 'src').should('include', this.expectedUpholsteredBlackBarStoolIMGSrc);
+
+}
+
+
+//Sold out alert box methods for assertions and clicks
+
+ClickUpholsteredWhiteChairSoldOut(){
+
+cy.get(this.upholsteredWhiteChair).click()
+
+}
+
+clickUpholsteredBlackBarStoolSoldOut(){
+
+cy.get(this.upholsteredBlackBarStool).click();
+
+}
+
+AssertSoldOutAlertBoxText(){
+
+cy.get(this.soldOutItemAlertBox).should('have.text', this.expectedSoldOutItemAlertBoxText01 + ' '
++ this.expectedSoldOutItemAlertBoxText02 + "'" + this.expectedSoldOutItemAlertBoxText03 + ' '
++ this.expectedSoldOutItemAlertBoxText04 + "'" + this.expectedSoldOutItemAlertBoxText05 + ' '
++ this.expectedSoldOutItemAlertBoxText06)
+.and('have.class', 'modal-sold-out-content')
+.and('be.visible');
+
+}
+
+clickContinueBtnOnSoldOutAlert(){
+
+    cy.get(this.ContinueBTnOnSoldOutAlert).click();
+}
+
+assertUserOnChairProductsPage(){
+
+cy.get(this.wickerChair).should('be.visible');
 
 }
 
